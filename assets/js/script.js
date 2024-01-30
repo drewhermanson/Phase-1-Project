b1f355be2c4058fb4d45b1aa16528a818f7cac33
Lotte-Et <charlotte.stowe@gmail.com>
2024-01-29 21:04:46 -0500

    Added function to get array of random valid city IDs

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var cityCodes = [];
+var cityNames = [];
+
+function getRandomCities() {  
+  for (var i = 0; i < 10; i++){
+    i; 
+  var num = Math.floor(Math.random() * 28065);
+  cityCodes.push(num);
+  }
+  }
+
+getRandomCities();
+
+console.log(cityCodes);
