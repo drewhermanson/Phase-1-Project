d9df58620106616b6d49b14a77f17a72a7c75ef3
Lotte-Et <charlotte.stowe@gmail.com>
2024-01-29 21:17:53 -0500

    Updated for a new city API

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,9 @@ var cityNames = [];
 function getRandomCities() {  
   for (var i = 0; i < 10; i++){
     i; 
-  var num = Math.floor(Math.random() * 28065);
+  var num = Math.floor(Math.random() * 4503);
   cityCodes.push(num);
   }
   }
 
 getRandomCities();
-
-console.log(cityCodes);
